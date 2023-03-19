1ee07c965a0c074fe8acbf245ecd1b0694aa9286
ajaxthebug <hey.bhupend@gmail.com>
2023-03-19 06:13:23 +0530

    started hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
+import styles from "../style.js";
 
-const Hero = () => {
-  return (
-    <div>Hero</div>
+const Hero = () => (
+    <section className={`flex md:flex-row flex-col ${styles.paddingY} `} id="home">
+
+      <div className={`flex-1 ${styles.flexStart}
+      flex-col xl:px-0 sm:px-16 px-6`}>
+
+
+
+      </div>
+
+    </section>
   )
-}
+
 
 export default Hero
