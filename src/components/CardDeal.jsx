39ca82082c3a10102bf827a7ed4546848696d124
ajaxthebug <hey.bhupend@gmail.com>
2023-03-22 02:51:51 +0530

    Added CardDeal Section

--- src/components/CardDeal.jsx
+++ src/components/CardDeal.jsx
@@ -1,9 +1,30 @@
 import React from 'react'
+import {card} from '../assets'
+import styles, {layout} from "../style.js";
+import Button from "./Button.jsx";
 
-const ClientDeal = () => {
-  return (
-    <div>ClientDeal</div>
+const CardDeal = () => (
+    <section className={layout.section}>
+
+        <div className={layout.sectionInfo}>
+          <h2 className={styles.heading2}>
+            Find a better card deal<br className="sm:block hidden"/> in few easy steps.
+          </h2>
+          <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
+            Finding a better card deal is now easier than ever. Simply compare rates and benefits to choose the best option.
+          </p>
+
+          <Button styles="mt-10"/>
+
+        </div>
+
+      <div className={layout.sectionImg}>
+          <img src={card} alt="card" className="w-[100%] h-[100%]"/>
+      </div>
+
+    </section>
+
   )
-}
 
-export default ClientDeal
+
+export default CardDeal
